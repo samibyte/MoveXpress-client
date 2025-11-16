494a6a68df006e0a49cc5c12968194af7f03831d
samibyte <samibyte.web@gmail.com>
2025-11-16 19:46:56 +0600

    Initialize project structure and add resources

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,5 @@
+const RootLayout = () => {
+  return <div>root</div>;
+};
+
+export default RootLayout;
---
+++ src/routes/router.tsx
@@ -0,0 +1,9 @@
+import RootLayout from "@/layouts/RootLayout";
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+  },
+]);
